8bb42c1b08854cf08b7f094985a3c0d71eed9669
Fede Fernández <720923+fedefernandez@users.noreply.github.com>
2023-05-27 09:41:24 +0200

    Models exercise solution cont

--- modules/common/shared/src/main/scala/scaladays/models/ids.scala
+++ modules/common/shared/src/main/scala/scaladays/models/ids.scala
@@ -2,15 +2,44 @@ package scaladays.models
 
 import java.util.UUID
 
+import cats.effect.Sync
+import cats.implicits.*
+
 import io.chrisdavenport.fuuid.FUUID
+import io.circe.generic.semiauto.*
+import io.circe.{Decoder as CirceDecoder, Encoder as CirceEncoder}
 
 object ids:
-
+  opaque type EventId  = FUUID
   opaque type PlayerId = FUUID
-  opaque type GameId = FUUID
+  opaque type GameId   = FUUID
+  opaque type Nickname = String
 
   object PlayerId:
-    def apply(id: FUUID): PlayerId = id
+    def apply(id: FUUID): PlayerId             = id
+    def apply[F[_]: Sync](): F[PlayerId]       = FUUID.randomFUUID
+    def unsafe(): PlayerId                     = FUUID.fromUUID(UUID.randomUUID)
+    def unapply(arg: String): Option[PlayerId] = FUUID.fromStringOpt(arg)
+
+    extension (playerId: PlayerId)
+      def value: FUUID = playerId
+      def show: String = value.show
 
   object GameId:
     def apply(id: FUUID): GameId = id
+    def unsafe(): GameId         = FUUID.fromUUID(UUID.randomUUID)
+
+    extension (gameId: GameId) def value: FUUID = gameId
+
+  object Nickname:
+    def apply(nickname: String): Nickname = nickname
+
+    extension (nickname: Nickname) def value: String = nickname
+
+  object EventId:
+
+    def apply(id: FUUID): EventId       = id
+    def apply[F[_]: Sync](): F[EventId] = FUUID.randomFUUID
+    def unsafe(): EventId               = FUUID.fromUUID(UUID.randomUUID)
+
+    extension (eventId: EventId) def value: FUUID = eventId
