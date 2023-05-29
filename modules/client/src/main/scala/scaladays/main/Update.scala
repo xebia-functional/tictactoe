4f564f31dd7f3c6acbd6524866d8badfecbd2c23
Fede Fernández <720923+fedefernandez@users.noreply.github.com>
2023-05-29 13:56:15 +0200

    WebSocker client <-> server exercise setup

--- modules/client/src/main/scala/scaladays/main/Update.scala
+++ modules/client/src/main/scala/scaladays/main/Update.scala
@@ -16,17 +16,21 @@ object Update:
   def impl[F[_]: Async](scalaDaysClient: ScalaDaysClient[F]): Update[F] = new Update[F]:
 
     override def update(model: Model[F]): Msg => (Model[F], Cmd[F, Msg]) =
+
       case msg @ Msg.UpdateNickname(nickname) =>
         (model.copy(nickname = nickname), Cmd.None)
 
-      case msg@Msg.LoginError(UnexpectedServerError(error)) =>
+      case msg @ Msg.LoginError(UnexpectedServerError(error)) =>
         (model.copy(errors = msg.error :: model.errors), Logger.errorOnce(s"Server error: $error"))
 
-      case msg@Msg.LoginError(err) =>
+      case msg @ Msg.LoginError(err) =>
         (model.copy(errors = err :: model.errors), Cmd.None)
 
-      case msg@Msg.LoginRequest(nickname) =>
+      case msg @ Msg.LoginRequest(nickname) =>
         (model.copy(nickname = nickname, player = Player.Waiting, errors = Nil), scalaDaysClient.getLogin(nickname))
 
-      case msg@Msg.LoginSuccess(playerId) =>
+      case msg @ Msg.LoginSuccess(playerId) =>
         (model.copy(player = Player.Registered(playerId), errors = Nil), Cmd.None)
+
+      case msg @ Msg.Logout =>
+        (Model.init, Cmd.None)
