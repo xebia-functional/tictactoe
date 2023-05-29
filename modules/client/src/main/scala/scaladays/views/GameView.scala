f7263514afaa635d0116cf2ac449b2c66605ef49
Fede Fernández <720923+fedefernandez@users.noreply.github.com>
2023-05-29 16:37:47 +0200

    Handle Game in client exercise solution

--- modules/client/src/main/scala/scaladays/views/GameView.scala
+++ modules/client/src/main/scala/scaladays/views/GameView.scala
@@ -34,4 +34,49 @@ object GameView:
       case (GameState.CrossTurn, Piece.Cross)   => true
       case _                                    => false
 
-    def CellView(position: Position) = ???
+    def CellView(position: Position) =
+      val newMovement = Movement(position, myPiece, false)
+      game.movements
+        .find(_.position == position).fold {
+          td(button(tpe := "button", cls := "btn btn-primary", MyTurnAtrr(myTurn)))
+        } { mov =>
+          td(button(tpe := "button", cls := s"btn btn-primary done ${mov.piece.cssClass}", disabled) {
+            if mov.confirmed then span(mov.piece.symbol)
+            else div(cls := "spinner-border text-light", Attribute("role", "status"))()
+          })
+        }
+
+    val restartButton: List[Html[Msg]] =
+      if(List(GameState.CrossWin, GameState.CircleWin, GameState.Tie).contains(game.state)) then
+        List(
+          div(cls := "d-grid gap-2 mt-4 d-sm-flex justify-content-sm-center")(
+            button(tpe := "button", cls := "btn btn-primary btn-lg px-4 gap-3")("Restart")
+          )
+        )
+      else List.empty[Html[Msg]]
+
+
+    div(
+      List(
+        p(cls := "lead mb-4")(message),
+        div(cls := "container")(
+          div(cls := "row align-items-center")(
+            div(cls := "col-2")(
+              div(cls := "label")("You"),
+              LabelView(myPiece)
+            ),
+            div(id := "board", cls := "col-8")(
+              table(
+                tr(CellView(Position(1, 1)), CellView(Position(1, 2)), CellView(Position(1, 3))),
+                tr(CellView(Position(2, 1)), CellView(Position(2, 2)), CellView(Position(2, 3))),
+                tr(CellView(Position(3, 1)), CellView(Position(3, 2)), CellView(Position(3, 3)))
+              )
+            ),
+            div(cls := "col-2")(
+              div(cls := "label")("Opponent"),
+              LabelView(opponentPiece)
+            )
+          )
+        )
+      ) ++ restartButton
+    )
