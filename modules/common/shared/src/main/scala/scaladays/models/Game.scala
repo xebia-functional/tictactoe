7690f0e1f36e122d7bd614430f7e2c5f26162f7c
Jack C. Viers <jackcviers@gmail.com>
2023-08-20 22:26:24 -0500

    Game modeling step 2 setup

--- modules/common/shared/src/main/scala/scaladays/models/Game.scala
+++ modules/common/shared/src/main/scala/scaladays/models/Game.scala
@@ -1,1 +1,18 @@
 package scaladays.models
+
+import scaladays.models.ids.*
+
+enum Piece:
+  case Cross, Circle
+
+final case class Position(x: Int, y: Int)
+
+object Position:
+  given Ordering[Position] = (x: Position, y: Position) => if (x.x == y.x) x.y - y.y else x.x - y.x
+
+enum GameState:
+  case CrossTurn, CircleTurn, Processing, CrossWin, CircleWin, Tie
+
+final case class Movement(position: Position, piece: Piece, confirmed: Boolean = true)
+
+final case class Game(gameId: GameId, crossPlayer: PlayerId, circlePlayer: PlayerId, state: GameState, movements: List[Movement])
--- modules/common/shared/src/main/scala/scaladays/models/ids.scala
+++ modules/common/shared/src/main/scala/scaladays/models/ids.scala
@@ -1,1 +1,11 @@
 package scaladays.models
+
+import java.util.UUID
+
+import io.chrisdavenport.fuuid.FUUID
+
+object ids:
+  type ??? = Any
+
+  opaque type PlayerId = ???
+  opaque type GameId   = ???
